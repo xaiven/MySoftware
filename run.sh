1e989b4fce39adc9eea1b062d1e5f1cbaa2cfcfc
Alex Sayenko <xaiven1990@gmail.com>
2025-09-06 03:22:00 +0300

    Add welcome() method

--- run.sh
+++ run.sh
@@ -2,4 +2,17 @@ git init
 git remote add origin https://github.com/<your-username>/my-python-app.git
 git add .
 git commit -m "Initial commit"
-git push -u origin master
+
+git checkout -b NewButton
+git push -u origin NewButton
+git checkout master
+git checkout -b NewScreen
+git push -u origin NewScreen
+git checkout NewButton
+
+# def click():
+#    print("Click")
+
+git add app.py
+git commit -m "Add click() method"
+git push
